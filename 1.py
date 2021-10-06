bd47b986666649e8d1e49fa8a4841208ed7b5ae1
Ihor Vasylets <44103737+SuitWhite@users.noreply.github.com>
2021-10-06 21:16:42 +0300

    Завдання 1, ПР, 8.10

--- 1.py
+++ 1.py
@@ -1,35 +1,42 @@
 class MyException(Exception): pass
-while True:
+
+def input_number():
+    while True:
         try:
-            a = []
-            print("Enter n:")
             n = int(input())
             if n<=0:
                 raise MyException
-                
-            print("Enter elements of array:")
-            for q in range(n):
-                a.append(int(input()))
-                
-            i = 0
-            lastplus = a[0]
-            while i < len(a):
-                if (a[i]>0):
-                    lastplus = a[i]
-                i += 1
-                
-            i = 0
-            dobutok = 1
-            while i < len(a):
-                if a[i] == lastplus:
-                    break
-                dobutok *= a[i]
-                i += 1
-                
-            print("Result: ", dobutok)
+            return n
         except MyException:
             print("N<1")
             continue
         except ValueError:
             print("Value should be int")
             continue
+                
+def array_init(n:int):
+    a = []
+    print("Enter elements of array:")
+    for q in range(n):
+        a.append(int(input()))
+    return a
+
+def count(array):
+    lastplus = array[0]
+    for i in range(len(array)):
+        if (array[i]>0):
+            lastplus = array[i]
+    dobutok = 1
+    for i in range(len(array)):
+        if array[i] == lastplus:
+            break
+        dobutok *= array[i]
+    return dobutok
+        
+
+while True:
+    n = input_number()
+    array = array_init(n)
+    dobutok = count(array)
+    print("Result: ", dobutok)
+        
